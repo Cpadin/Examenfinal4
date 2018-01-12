da806bf8c0ac2067ead18416f41e26e7eabbef7b
Carolina Padin <caropadin@gmail.com>
2018-01-12 02:59:58 -0300

    errores corregidos: la tecla de signo negativo tambien cambia el signo a los operandos que son resultado de una operacion anterior. Al clickear la tecla ON se borran los operandos y resultados de operaciones anteriores Las operaciones funcionan las cuatro correctamente y al presionar la tecla igual se muestra el resultado. si se vuelve a presionar el igual se repetira la operacion tomando como primer operador al resultado de la cuenta y como segundo al mismo de la cuenta anterior. Se pueden concatenar operaciones con el resultado de la operacion anterior. Las variables internas que definian una funcion autoejecutable no eran necesarias. ya todo el codigo esta dentro de una funcion autoejecutable.
    Preguntar como es el tema de las 8 cifras en los resultados. No tiene sentido cortar el numero si no fueran los decimales. Podria usar toPrecision()

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-//podria a las funciones autoejecutables sacarles eso y ponerlas en un objeto init para inicializarlas
+// falta hacer que los resultados no superen las 8 cifras
   var Calculadora = ( function(){
     var pantalla = document.getElementById('display');
     var todasTeclas = document.getElementsByClassName('tecla');
@@ -12,92 +12,88 @@
     var teclaMultiplica = document.getElementById('por');
     var teclaDivide = document.getElementById('dividido');
     var teclaIgual = document.getElementById('igual');
-    var num1 = null;
-    var num2 = null;
-    var resultado = null;
-
+    var num1;
+    var num2;
+    var resultado;
   // mostrar graficamente la presion en las teclas
-    var apretarSoltarTeclas = ( function (){
-        for (var i=0; i<todasTeclas.length; i++){
-        todasTeclas[i].onmousedown = function reducirTecla() {
-          this.setAttribute('style', 'transform: scale(0.95,0.95)');}
-        todasTeclas[i].onmouseup = function normalizarTecla() {
-          this.setAttribute('style', 'transform: scale(1,1)');}
-      }
-    })();
-  // mostrar los numeros presionados en la pantalla de la calculadora sin superar las 8 cifras y sin empezar con cero(s)
-    var displayNumeros = ( function (){
-        for (var i = 0; i < teclasNumericas.length; i++) {
-          teclasNumericas[i].onclick = function imprimirValor() {
-            if (pantalla.innerHTML == '0')
-              pantalla.innerHTML = "";
-            if (pantalla.innerHTML.length < 8) {
-              pantalla.innerHTML += teclasNumericas.indexOf(this);
-            }
-          };
+    for (var i=0; i<todasTeclas.length; i++){
+    todasTeclas[i].onmousedown = function reducirTecla() {
+      this.setAttribute('style', 'transform: scale(0.95,0.95)');}
+    todasTeclas[i].onmouseup = function normalizarTecla() {
+      this.setAttribute('style', 'transform: scale(1,1)');}
+    }
+  // mostrar los numeros presionados en la pantalla de la calculadora sin permitir superar las 8 cifras y sin empezar con cero(s)
+    for (var i = 0; i < teclasNumericas.length; i++) {
+      teclasNumericas[i].onclick = function imprimirValor() {
+        if (pantalla.innerHTML == '0')
+          pantalla.innerHTML = "";
+        if (pantalla.innerHTML.length < 8) {
+          pantalla.innerHTML += teclasNumericas.indexOf(this);
         }
-      })();
-  // al presionar la tecla ON se borra el contenido de la pantalla y queda en cero
-    var limpiarPantalla = (function (){
-      teclaOn.onclick = function (){
-        pantalla.innerHTML = '0';
-        };
-      })();
+      };
+    }
+  // al presionar la tecla ON se borra el contenido de la pantalla y queda en cero.Ademas el resultado de la operacion anterior y el segundo operando anterior son borrados.
+    teclaOn.onclick = function (){
+      pantalla.innerHTML = '0';
+      num1 = undefined;
+      num2 = undefined;
+      };
   // colocar un unico separador decimal
-   var separadorDecimal = (function (){
-      teclaPunto.onclick = function (){
-        if (pantalla.innerHTML.indexOf(".") == -1) {
-          pantalla.innerHTML += '.';
-          };
-        }
-      })();
-  // asignar signo positivo o negativo siempre y cuando no haya un cero en la pantalla
-    var signo = (function (){
-        teclaSigno.onclick = function () {
-          if (pantalla.innerHTML != '0') {
-            if (pantalla.innerHTML.charAt(0) == '-') {
-              pantalla.innerHTML = pantalla.innerHTML.slice(1);
-            } else {
-              pantalla.innerHTML = '-' + pantalla.innerHTML;
-            }
-          }
-        };
-        })();
-
-    var operaciones = (function (){
-      function sumar(num1, num2){
-        return num1 + num2;
-      };
-      function restar(num1, num2){
-        return num1 - num2;
-      };
-      function multiplicar(num1, num2){
-        return num1 * num2;
-      };
-      function dividir(num1, num2){
-        return num1 / num2;
-      };
-      for (var i = 0; i < teclasOperadoras.length; i++) {
-        teclasOperadoras[i].onclick = function captarNumero(){
-          var self = this;
-          if (num1==undefined) {
-             num1 =  Number(pantalla.innerHTML);
-          }
-        pantalla.innerHTML = '';
-        teclaIgual.onclick = function mostrarResultado(){
-          num2 = Number(pantalla.innerHTML);
-          if (self == teclaSuma) {
-            resultado = sumar(num1, num2);
-            } else if (self == teclaResta) {
-              resultado = restar(num1, num2);
-            } else if (self == teclaMultiplica) {
-              resultado = multiplicar(num1, num2);
-            } else if (self == teclaDivide) {
-              resultado = dividir(num1, num2);
-            }
-          pantalla.innerHTML = resultado;
-          };
+    teclaPunto.onclick = function (){
+      if (pantalla.innerHTML.indexOf(".") == -1) {
+        pantalla.innerHTML += '.';
         };
       }
-    })();
+  // asignar signo positivo o negativo siempre y cuando no haya un cero en la pantalla. Ademas si hay un numero almacenado como resultado de la operacion anterior, que se almacene nuevamente con el cambio de signo indicado.
+    teclaSigno.onclick = function () {
+      if (pantalla.innerHTML != '0') {
+        if (pantalla.innerHTML.includes('-')) {
+          pantalla.innerHTML = pantalla.innerHTML.slice(1);
+        } else {
+          pantalla.innerHTML = '-' + pantalla.innerHTML;
+        }
+        if (num1 != undefined) {
+          num1 = num1 * -1;
+        }
+      }
+    };
+// funcionalidad de la calculadora: suma, resta, multiplicacion, division y mostrar resultado
+    function sumar(num1, num2){
+      return num1 + num2;
+    };
+    function restar(num1, num2){
+      return num1 - num2;
+    };
+    function multiplicar(num1, num2){
+      return num1 * num2;
+    };
+    function dividir(num1, num2){
+      return num1 / num2;
+    };
+    for (var i = 0; i < teclasOperadoras.length; i++) {
+      teclasOperadoras[i].onclick = function captarNumeroYOperar(){
+        var self = this;
+        var num2 = undefined;
+        if (num1 == undefined) {
+           num1 =  Number(pantalla.innerHTML);
+        }
+      pantalla.innerHTML = '';
+      teclaIgual.onclick = function mostrarResultado(){
+        if (num2==undefined) {
+        num2 = Number(pantalla.innerHTML);
+        }
+          if (self == teclaSuma) {
+          resultado = sumar(num1, num2);
+          } else if (self == teclaResta) {
+            resultado = restar(num1, num2);
+          } else if (self == teclaMultiplica) {
+            resultado = multiplicar(num1, num2);
+          } else if (self == teclaDivide) {
+            resultado = dividir(num1, num2);
+          }
+        pantalla.innerHTML = resultado;
+        num1 =  Number(pantalla.innerHTML);
+        };
+      };
+    }
 })();
