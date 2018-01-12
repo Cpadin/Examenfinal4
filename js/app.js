1f86affa2b8395355ead8bc5b2337d55b8ebc7af
Carolina Padin <caropadin@gmail.com>
2018-01-12 20:43:30 -0300

    en la cuenta de la vaidacion de 8 son 8 caracteres, no 8 numeros me dice el tutor. asique el punto que separa la parte decimal tambien cuenta. tambien me dicen que tengo que respetar las 8 cifras en los resultados aunque no tenga sentido el resultado extrayendole cifras. Asi respeto la consigna.
    Errores corregidos:
    si hago click a un operador sin haber ingresado un nro antes que tome el cero como primer valor para poder operar con el segundo
    
    Terminado eso y chequeado todo.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-// falta hacer que los resultados no superen las 8 cifras
   var Calculadora = ( function(){
     var pantalla = document.getElementById('display');
     var todasTeclas = document.getElementsByClassName('tecla');
@@ -12,9 +11,11 @@
     var teclaMultiplica = document.getElementById('por');
     var teclaDivide = document.getElementById('dividido');
     var teclaIgual = document.getElementById('igual');
-    var num1;
+    var num1 = 0;
     var num2;
+    var operador;
     var resultado;
+    var ultimaTeclaApretada;
   // mostrar graficamente la presion en las teclas
     for (var i=0; i<todasTeclas.length; i++){
     todasTeclas[i].onmousedown = function reducirTecla() {
@@ -27,15 +28,14 @@
       teclasNumericas[i].onclick = function imprimirValor() {
         if (pantalla.innerHTML == '0')
           pantalla.innerHTML = "";
-        if (pantalla.innerHTML.length < 8) {
+        if (pantalla.innerHTML.length < 8)
           pantalla.innerHTML += teclasNumericas.indexOf(this);
-        }
       };
     }
   // al presionar la tecla ON se borra el contenido de la pantalla y queda en cero.Ademas el resultado de la operacion anterior y el segundo operando anterior son borrados.
     teclaOn.onclick = function (){
       pantalla.innerHTML = '0';
-      num1 = undefined;
+      num1 = 0;
       num2 = undefined;
       };
   // colocar un unico separador decimal
@@ -52,7 +52,7 @@
         } else {
           pantalla.innerHTML = '-' + pantalla.innerHTML;
         }
-        if (num1 != undefined) {
+        if (num1 != 0) {
           num1 = num1 * -1;
         }
       }
@@ -71,29 +71,44 @@
       return num1 / num2;
     };
     for (var i = 0; i < teclasOperadoras.length; i++) {
-      teclasOperadoras[i].onclick = function captarNumeroYOperar(){
-        var self = this;
-        var num2 = undefined;
-        if (num1 == undefined) {
-           num1 =  Number(pantalla.innerHTML);
-        }
-      pantalla.innerHTML = '';
-      teclaIgual.onclick = function mostrarResultado(){
-        if (num2==undefined) {
-        num2 = Number(pantalla.innerHTML);
-        }
-          if (self == teclaSuma) {
-          resultado = sumar(num1, num2);
-          } else if (self == teclaResta) {
-            resultado = restar(num1, num2);
-          } else if (self == teclaMultiplica) {
-            resultado = multiplicar(num1, num2);
-          } else if (self == teclaDivide) {
-            resultado = dividir(num1, num2);
-          }
-        pantalla.innerHTML = resultado;
-        num1 =  Number(pantalla.innerHTML);
-        };
+    teclasOperadoras[i].onclick = function getOperadorAndNum1() {
+      num1 = Number(pantalla.innerHTML);
+      operador = this;
+      num2 = undefined;
+      pantalla.innerHTML = "";
+      ultimaTeclaApretada = 'operador';
       };
     }
+    teclaIgual.onclick = function getNum2YOperar() {
+      ultimaTeclaApretada = 'igual';
+      if (num2==undefined) {
+      num2 = Number(pantalla.innerHTML);
+       }
+      if (operador == teclaSuma) {
+        resultado = sumar(num1, num2);
+      } else if (operador == teclaResta) {
+        resultado = restar(num1, num2);
+      } else if (operador == teclaMultiplica) {
+        resultado = multiplicar(num1, num2);
+      } else if (operador == teclaDivide) {
+        resultado = dividir(num1, num2);
+      }
+      pantalla.innerHTML = resultado.toString().substring(0,8);
+      num1 = resultado;
+    // al apretar una tecla numerica, ya habiendo un resultado en la pantalla, este resultado desaparece y se toma como primer operando el nuevo que se ingresa, comenzando una nueva operacion.
+      for (var i = 0; i < teclasNumericas.length; i++) {
+        teclasNumericas[i].onclick = function (){
+          if (ultimaTeclaApretada == 'igual') {
+            pantalla.innerHTML = "";
+            ultimaTeclaApretada = undefined;
+            num1 = undefined;
+            num2 = undefined;
+          }
+          if (pantalla.innerHTML == '0')
+            pantalla.innerHTML = "";
+          if (pantalla.innerHTML.length < 8)
+            pantalla.innerHTML += teclasNumericas.indexOf(this);
+        }
+      }
+    };
 })();
